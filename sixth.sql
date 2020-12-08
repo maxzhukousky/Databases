777d7991180fd39c2fb3ff20e67f6662640db627
maxzhukousky <47918418+maxzhukousky@users.noreply.github.com>
2020-12-09 02:50:19 +0300

    Add files via upload

--- sixth.sql
+++ sixth.sql
@@ -1,6 +1,6 @@
 SELECT DISTINCT 
 COUNT(CASE WHEN S.name LIKE 
-CONCAT('%',LEFT('Жуковский',6),'%') THEN S.name ELSE NULL END) AS 'Похожие с одной буквой',
+CONCAT('%',LEFT('Максим',6),'%') THEN S.name ELSE NULL END) AS 'Похожие с одной буквой',
 COUNT(CASE WHEN S.name LIKE 
 CONCAT('%',LEFT('Максим',5),'%') THEN S.name ELSE NULL END) AS 'Похожие с двумя буквами'
 FROM StudentsRB.name2 as S;
