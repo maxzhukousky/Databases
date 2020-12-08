f61f86c1cb5911468d992b70dac7e64929fd3d9f
maxzhukousky <47918418+maxzhukousky@users.noreply.github.com>
2020-12-09 01:47:18 +0300

    Add files via upload

--- fourth.sql
+++ fourth.sql
@@ -3,6 +3,6 @@ WHERE name = 'Жуковский';
 
 SELECT name AS 'Имя' FROM StudentsRB.name2 WHERE name = 'Максим';
 
-SELECT F.name AS 'Фамилия', L.name AS 'Имя' FROM StudentsRB.name1 as F join StudentsRB.name2 as L
+SELECT F.name AS 'Фамилия', L.name'Имя' FROM StudentsRB.name1 as F join StudentsRB.name2 as L
 WHERE F.name = ' Жуковский ' and L.name = ' Максим';
 
